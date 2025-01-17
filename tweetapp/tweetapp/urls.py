7d8a12d22ab681fbda6addade51c2ee55ce3cd72
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2025-01-17 18:46:52 -0500

    Application initial template file created referencing the parent layout.html

--- tweetapp/tweetapp/urls.py
+++ tweetapp/tweetapp/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
