ffe93595558122d3ad8e6fd532d6ecc525e84c03
Sushanth Nandeti <sushanthnandeti96@gmail.com>
2025-01-19 02:57:45 -0500

    Authentication (User Registration, login and logout) added with djangi user model

--- tweetapp/tweet/views.py
+++ tweetapp/tweet/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
-
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
+from .forms import UserRegistrationForm
 # Create your views here.
 
 def index(request):
@@ -12,6 +14,7 @@ def tweet_list(request):
     tweets = Tweet.objects.all().order_by("-created_at")
     return render(request, 'tweet_list.html', {'tweets': tweets})
 
+@login_required
 def tweet_create(request):
 
     if request.method == 'POST': 
@@ -26,6 +29,7 @@ def tweet_create(request):
     
     return render(request, 'tweet_form.html', {'form' : form})
 
+@login_required
 def tweet_edit(request, tweet_id):
     
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
@@ -40,7 +44,7 @@ def tweet_edit(request, tweet_id):
         form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form' : form})
 
-
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     
@@ -48,3 +52,17 @@ def tweet_delete(request, tweet_id):
         tweet.delete()
         return redirect('tweet_list')
     return render(request, 'tweet_confirm_delete.html', {'tweet' : tweet})
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password1'])
+            user.save()
+            login(request, user)
+            return redirect('tweet_list')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'registration/register.html', {'form' : form})
